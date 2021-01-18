411c8d9e630de898eb58708f9c4843824afcc6c7
Arenzon <atlantidka1@gmail.com>
2021-01-18 23:32:54 +0300

    Initial commit

---
+++ money.java
@@ -0,0 +1,8 @@
+public class Money {
+    public static void main(String[] args) {
+        int account = 2_000_000_000;
+        int contribution = 500_000_000;
+        int total = account + contribution;
+        System. out. println(total);
+    }
+}
